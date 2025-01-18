ffe774d659fa118779a88084a5c37eb561aa12c4
Dang_Thang <ddthang.workspace@gmail.com>
2025-01-18 23:53:20 +0700

    fix logout

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,7 +3,7 @@
     ini_set('display_errors', '1');
     require_once 'config/config.php';
     require_once 'helpers/errors.php';
-    require_once 'helpers/email.php';
+    // require_once 'helpers/email.php';
     require_once 'helpers/session.php';
     require_once 'helpers/redirect.php';
     require_once 'helpers/auth.php';
